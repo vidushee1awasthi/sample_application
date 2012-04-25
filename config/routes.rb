c943b936429c062b76fe402dbdc95847e19d18a3
vidushee <vidusheeawasthi19@gmail.com>
2012-04-25 14:19:57 +0530

    root path set, index.html removed, database.yml removed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ SampleApp::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
 
+  root :to => 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
