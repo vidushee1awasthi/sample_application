8a46c2c7693b2827c702900dac52f7d70d52288c
vidushee <vidusheeawasthi19@gmail.com>
2012-04-25 14:33:01 +0530

    links added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "static_pages/home", :as => :home
 
-  get "static_pages/help"
-  get "static_pages/about"
+  get "static_pages/help", :as => :help
+  get "static_pages/about", :as => :about
 
   root :to => 'static_pages#home'
 
